9bd96ee1b1ea84dc17cb8e5583b06bb63856f305
刘述 <383883040@qq.com>
2020-07-31 17:33:15 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const style = {
+  textAlign: 'center'
 }
-
-export default App;
+const divStyle = {
+  background: 'rgba(47, 47, 47, 0.98)',
+  height: '50px',
+  lineHeight: '50px',
+  color: 'white'
+}
+const inputStyle = {
+  marginLeft: '50px'
+}
+class Test extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      doneList: [],
+      doingList: []
+    }
+    this.cacheValue = ''
+  }
+  change = (e) => {
+    this.setState({
+      value: e.target.value
+    })
+  }
+  onKeyup = (e) => {
+    if (e.keyCode === 13) {
+      const arr = [...this.state.doingList]
+      arr.push(this.state.value)
+      this.setState({
+        doingList: arr,
+        value: ''
+      })
+    }
+  }
+  render () {
+    const doingList = []
+    this.state.doingList.forEach((item, index) => {
+      doingList.push(<li key={index}>{item}</li>)
+    })
+    return (
+      <div
+        style={style} >
+        <div
+          style={divStyle}
+        >
+          ToDoList
+          <input
+            style={inputStyle}
+            type="text"
+            value={this.state.value}
+            onChange={(event) => { this.change(event) }}
+            onKeyUp={this.onKeyup} />
+        </div>
+        <h3>正在进行</h3>
+        <ul>
+          {doingList}
+        </ul>
+      </div>)
+  }
+}
+export default Test;
