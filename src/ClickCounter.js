3592b41ab159dbf7ef1800f14ec11018c978452d
刘述 <383883040>
2020-12-07 21:50:55 +0800

    提交代码

---
+++ src/ClickCounter.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+class ClickCounter extends Component {
+  constructor(props) {
+    super(props)
+    this.onClickButton = this.onClickButton.bind(this)
+    this.state = { count: 0 }
+  }
+  onClickButton () {
+    this.setState({ count: this.state.count + 1 })
+  }
+  render () {
+    const countStyle = {
+      margin: '16px'
+    }
+    return (
+      <div
+        style={countStyle}>
+        <button
+          onClick={this.onClickButton}>
+          Click Me
+        </button>
+        <div>
+          Click Count:{this.state.count}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ClickCounter
